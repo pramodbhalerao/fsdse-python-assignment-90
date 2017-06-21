45757ff5a0abfde03b967bb1108bd9225fde5802
pramodbhalerao <pramodbhalerao_01@yahoo.com>
2017-06-21 14:44:41 +0000

    Done

--- build.py
+++ build.py
@@ -2,18 +2,22 @@ import pandas as pd
 
 
 def equal_operator(ds1, ds2):
-    """
-    Enter your code here
-    """
+    ds = (ds1 == ds2)
+    return ds
 
 
 def greater_than_operator(ds1, ds2):
-    """
-    Enter your code here
-    """
+    ds = (ds1 > ds2)
+    return ds
 
 
 def less_than_operator(ds1, ds2):
-    """
-    Enter your code here
-    """
+    ds = (ds1 < ds2)
+    return ds
+
+input1 = pd.Series([2, 4, 6, 8, 10])
+input2 = pd.Series([1, 3, 5, 7, 10])
+
+equal_operator(input1,input2)
+greater_than_operator(input1,input2)
+less_than_operator(input1,input2)
